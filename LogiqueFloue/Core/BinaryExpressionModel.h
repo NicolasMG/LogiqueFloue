82ded6e8e707bce2ccdd1bea110eadda56fa3765
LouisPlanes <louis.planes@gmail.com>
2018-03-22 11:04:25 +0100

    finition des unary et binary expression Model

--- LogiqueFloue/Core/BinaryExpressionModel.h
+++ LogiqueFloue/Core/BinaryExpressionModel.h
@@ -4,6 +4,7 @@ namespace core
 	template<typename T>
 	class BinaryExpressionModel : public BinaryExpression, public Expression {
 	public:
+
 		virtual T evaluate(expression l, Expression r) {
 			if (l == null) throw new NullExpressionException();			if (r == null) throw new NullExpressionException();			if (_operator == null) throw new NullOperatorException();			return _operator.evaluate(l, r)
 
@@ -15,17 +16,51 @@ namespace core
 		}
 
 
-		virtual BinaryExpressionModel const getLeft();
-		virtual BinaryExpressionModel const getRight();
-		virtual BinaryExpression const getOperator();
+		virtual &BinaryExpressionModel<T> const getLeft();
+		virtual &BinaryExpressionModel<T> const getRight();
+		virtual &BinaryExpression<T> const getOperator();
 
-		virtual void setLeft(BinaryExpressionModel&);
-		virtual void setRight(BinaryExpressionModel&);
-		virtual void setOperator(BinaryExpression&);
+		virtual void setLeft(const BinaryExpressionModel<T>&);
+		virtual void setRight(const BinaryExpressionModel<T>&);
+		virtual void setOperator(const BinaryExpression<T>&);
 
 	private:
-		BinaryExpressionModel left;
-		BinaryExpressionModel right;
-		BinaryExpression _operator;
+		BinaryExpressionModel<T> left;
+		BinaryExpressionModel<T> right;
+		BinaryExpression<T> _operator;
 	};
 }
+
+template<T>
+&BinaryExpressionModel<T> BinaryExpressionModel::getLeft() {
+	return left;
+}
+template<T>
+&BinaryExpressionModel<T> BinaryExpressionModel::getRight() {
+	return left;
+}
+template<typename T>
+inline &BinaryExpression const core::BinaryExpressionModel<T>::getOperator()
+{
+	return _operator;
+}
+
+template<typename T>
+inline void core::BinaryExpressionModel<T>::setLeft(const BinaryExpressionModel<T> & _left)
+{
+	left = _left;
+}
+
+template<typename T>
+inline void core::BinaryExpressionModel<T>::setRight(const BinaryExpressionModel<T> & _right)
+{
+	right = _right
+}
+
+template<typename T>
+inline void core::BinaryExpressionModel<T>::setOperator(const BinaryExpression<T> & ope)
+{
+	_operator = ope;
+}
+
+
--- LogiqueFloue/Core/UnaryExpressionModel.h
+++ LogiqueFloue/Core/UnaryExpressionModel.h
@@ -15,8 +15,41 @@ namespace core
 			return evaluate(operand);
 
 		}
+
+		virtual &Expression<T> const getOperand();
+		virtual &UnaryExpression<T> const getOperator();
+
+		virtual void setOperand(const Expression<T>&);
+		virtual void setOperator(const UnaryExpression<T>&);
+
+
 	private:
 		Expression operand;
 		UnaryExpression _operator;
 	};
+
+	template<typename T>
+	inline &Expression<T> const UnaryExpressionModel<T>::getOperand()
+	{
+		return operand;
+	}
+
+	template<typename T>
+	inline &UnaryExpression<T> const UnaryExpressionModel<T>::getOperator()
+	{
+		return _operator;
+	}
+
+	template<typename T>
+	inline void UnaryExpressionModel<T>::setOperand(const Expression<T>& _operand)
+	{
+		operand = _operand;
+	}
+
+	template<typename T>
+	inline void UnaryExpressionModel<T>::setOperator(const UnaryExpression<T>& ope)
+	{
+		_operator = ope;
+	}
+
 }
